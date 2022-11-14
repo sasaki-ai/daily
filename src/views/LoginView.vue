ba80ae02b67a03e84347dd05c0aa80c1011e2dae
sasaki <yzj1326668766@gmail.com>
2022-11-14 19:53:08 +0800

    正式版

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,5 @@
 <template>
+  <n-spin :show="show">
     <n-grid cols="12" item-responsive>
       <n-grid-item span="12">
         <n-card class="bgLogin" :bordered="false">
@@ -33,6 +34,7 @@
         </n-card>
       </n-grid-item>
     </n-grid>
+  </n-spin>
 </template>
 
 <script setup>
@@ -40,8 +42,13 @@ import { WebviewWindow } from '@tauri-apps/api/window';
 import { onMounted, ref } from "@vue/runtime-core";
 import { invoke } from "@tauri-apps/api/tauri";
 import { adminLogin, userLogin } from '../hooks/login';
-import { useMessage } from 'naive-ui';
+import { useMessage, useDialog } from 'naive-ui';
+import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
+import { relaunch } from '@tauri-apps/api/process'
+import { onUpdaterEvent } from "@tauri-apps/api/updater";
 
+const dialog = useDialog();
+let show = ref(false)
 const message = useMessage()
 let user = ref({
   uname: "",
@@ -76,6 +83,10 @@ let rules = ref({
   }
 })
 
+onMounted(async () => {
+  await updateApp()
+})
+
 const loginUser = async () => {
   let logindata = await userLogin(user.value);
   if (logindata.data.status == 200) {
@@ -103,6 +114,54 @@ const loginAdmin = async () => {
   }
 }
 
+const check = async () => {
+  try {
+    const { shouldUpdate, manifest } = await checkUpdate()
+    if (shouldUpdate) {
+      message.info("可更新,最新版为:" + manifest.version)
+    } else {
+      message.success("当前为最新版本")
+    }
+  } catch (error) {
+    message.error("更新异常:" + error)
+  }
+}
+const updateApp = async () => {
+  try {
+    //检查是否有可用的更新 返回UpdateResult
+    const { shouldUpdate, manifest } = await checkUpdate();
+    message.info("当前版本:" + manifest.version);
+    if (shouldUpdate) {
+      dialog.warning({
+        title: '更新',
+        content: '是否更新?',
+        positiveText: '是',
+        negativeText: '否',
+        onPositiveClick: async () => {
+          show.value = true;
+          // 如果有可用的更新，请安装更新
+          await installUpdate()
+          const unlisten = await onUpdaterEvent(async ({ error, status }) => {
+            if (status == "PENDING") {
+              message.info("下载开始")
+            } else if (status == "DONE") {
+              message.success("安装完成")
+              // 安装完成后重启
+              await relaunch()
+            } else if (status == "ERROR") {
+              message.error("安装失败:" + error);
+            }
+          });
+          unlisten();
+          show.value = false;
+        }
+      })
+
+    }
+  } catch (error) {
+    alert("更新异常:" + error)
+  }
+}
 </script>
 
 <style scoped>
