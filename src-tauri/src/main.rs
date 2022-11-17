672e5c2bbd6091b95685ab230c70548de9c7bf25
sasaki <yzj1326668766@gmail.com>
2022-11-18 00:52:21 +0800

    添加托盘

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,50 +3,50 @@
     windows_subsystem = "windows"
 )]
 use tauri::Manager;
+use tauri::{ SystemTray, SystemTrayMenu};
 
 //让前端调用的命令
 #[tauri::command]
 //打开主窗口
 async fn open_main(handle: tauri::AppHandle) {
-    tauri::WindowBuilder::new(
-        &handle,
-        "main",
-        tauri::WindowUrl::App("/home".into()),
-    )
-    .inner_size(800.0, 600.0)
-    .title("日常")
-    .center()
-    .build()
-    .unwrap();
+    tauri::WindowBuilder::new(&handle, "main", tauri::WindowUrl::App("/home".into()))
+        .inner_size(800.0, 600.0)
+        .title("日常")
+        .center()
+        .build()
+        .unwrap();
 }
 
 #[tauri::command]
 async fn open_admin(handle: tauri::AppHandle) {
-    tauri::WindowBuilder::new(
-        &handle,
-        "admin",
-        tauri::WindowUrl::App("/admin".into()),
-    )
-    .inner_size(800.0, 600.0)
-    .title("控制面板")
-    .center()
-    .build()
-    .unwrap();
+    tauri::WindowBuilder::new(&handle, "admin", tauri::WindowUrl::App("/admin".into()))
+        .inner_size(800.0, 600.0)
+        .title("控制面板")
+        .center()
+        .build()
+        .unwrap();
 }
 
 #[tauri::command]
 async fn close_splashscreen(window: tauri::Window) {
-  // Close splashscreen
-  if let Some(splashscreen) = window.get_window("open") {
-    splashscreen.close().unwrap();
-  }
-  // Show main window
-  window.get_window("login").unwrap().show().unwrap();
+    // Close splashscreen
+    if let Some(splashscreen) = window.get_window("open") {
+        splashscreen.close().unwrap();
+    }
+    // Show main window
+    window.get_window("login").unwrap().show().unwrap();
 }
 
 fn main() {
+    let tray_menu = SystemTrayMenu::new(); // insert the menu items here
+    let system_tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![open_main,open_admin,close_splashscreen])
+        .system_tray(system_tray)
+        .invoke_handler(tauri::generate_handler![
+            open_main,
+            open_admin,
+            close_splashscreen
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
