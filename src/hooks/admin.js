526ea9b96997650a5013772dbf3e1f39665e7b26
sasaki <yzj1326668766@gmail.com>
2022-11-25 19:29:19 +0800

    修改添加账单时间问题

--- src/hooks/admin.js
+++ src/hooks/admin.js
@@ -6,7 +6,7 @@ export function confirmBills(bids) {
     return api.put('admin/confirm',bids)
 }
 export function delBills(bids) {
-    return api.delete('admin/cancel',{data: bids})
+    return api.delete('admin/cancel',bids)
 }
 export function createUser(user) {
     return api.post('admin/create/user',user)
